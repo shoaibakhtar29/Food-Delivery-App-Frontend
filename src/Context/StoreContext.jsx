746d0c030ec4a5bac24ce94305f575f77355cc82
Shoaib Akhtar <shoaibakhtar2910@gmail.com>
2024-07-11 01:16:15 +0530

    reinitialize

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
-    const url = "http://localhost:4000";
+    const url = "https://food-delivery-app-backend-hyvf.onrender.com";
     const [token, setToken] = useState("");
 
     const [food_list, setFoodList] = useState([]);
